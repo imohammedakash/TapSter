58c370170f2f11dac2929ec9ca099a3bdfe87e07
imohammedakash <mdakash2003evergreen@gmail.com>
2023-09-19 00:36:02 +0530

    Register developed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
-import store from "./Redux/store";
+import store from "../component/Redux/store";
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
