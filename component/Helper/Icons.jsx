1d8df7dd211e7999f4cbd6dfa75c67477c974668
imohammedakash <mdakash2003evergreen@gmail.com>
2023-10-11 23:47:32 +0530

    Updated Icons component with target blank

--- component/Helper/Icons.jsx
+++ component/Helper/Icons.jsx
@@ -4,9 +4,10 @@ const Icons = ({ link, bg }) => {
   return (
     <a
       href={link}
+      target="_blank"
       className="h-16 w-16 flex items-center justify-center text-xl rounded-xl mt-5 shadow-xl bg-white border p-1"
     >
-      <img src={bg} target="_blank" className="h-full w-full object-cover rounded-xl" alt={bg} />
+      <img src={bg} className="h-full w-full object-cover rounded-xl" alt={bg} />
     </a>
   );
 };
