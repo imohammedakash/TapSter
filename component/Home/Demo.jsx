1cee7aaa53518bbd3edbf7ffd91bdbf317700fd0
imohammedakash <mdakash2003evergreen@gmail.com>
2023-10-13 21:59:54 +0530

    Deployment bug for Designation  resolved

--- component/Home/Demo.jsx
+++ component/Home/Demo.jsx
@@ -55,7 +55,7 @@ const Demo = ({ userData }) => {
                   <MdVerified className="text-green-400" />
                 </h2>
                 <h3 className="text-[0.68rem] font-xs font-normal text-center text-white mt-1">
-                  {userData.designation !== "" && userData?.company !== ""
+                  {userData?.designation !== "" && userData?.company !== ""
                     ? userData?.designation + " | " + userData?.company
                     : "Full Stack Developer | Designer"}
                 </h3>
