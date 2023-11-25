74001ec862fa5c5b2e77bf39186af443e7299d99
imohammedakash <mdakash2003evergreen@gmail.com>
2023-11-25 11:35:10 +0530

    Cart Page Developed

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const index = () => {
-  const user = useSelector((state) => state?.user?.user);
   return (
     <>
       <Home />
